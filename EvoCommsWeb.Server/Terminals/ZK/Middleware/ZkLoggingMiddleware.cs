29c4961648031fd0a1fb8ca3aa2bfcdc26071224
Dylan Marriott <dylan.marriott@clockingsystems.co.uk>
2024-12-31 11:43:04 +0000

    Feat: Add ZkLoggingMiddleware for device request logging
    Introduced middleware to log incoming requests to the "/iclock" endpoint. This includes buffering and logging the request body for debugging purposes, ensuring the request stream remains intact for further processing.

---
+++ EvoCommsWeb.Server/Terminals/ZK/Middleware/ZkLoggingMiddleware.cs
@@ -0,0 +1,18 @@
+namespace EvoCommsWeb.Server.Terminals.ZK.Middleware;
+
+public class ZkLoggingMiddleware(
+    RequestDelegate next,
+    ILogger<ZkLoggingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Path.StartsWithSegments("/iclock"))
+        {
+            context.Request.EnableBuffering();
+            var body = await new StreamReader(context.Request.Body).ReadToEndAsync();
+            logger.LogDebug($"Device Request: {body}");
+            context.Request.Body.Position = 0;
+        }
+        await next(context);
+    }
+}
